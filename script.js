7c813a0511e90b40e10246d34d24922a07c21a27
fuuluw <85565290+fuuluw@users.noreply.github.com>
2022-01-30 16:32:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["BSc in Mathemaical science graduate", "Statistics graduate", "Computer Science graduate"],
+        strings: ["BSc in Mathematical science graduate", "Statistics graduate", "Computer Science graduate"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["BSc in Mathemaical science graduate", "Statistics graduate", "Computer Science graduate"],
+        strings: ["BSc in Mathematical science graduate", "Statistics graduate", "Computer Science graduate"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
